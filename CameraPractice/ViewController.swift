7c51571623cb289e7807a25f24f938956355c433
Casssie <cassieareff@gmail.com>
2019-06-24 16:37:52 -0400

    first commit

--- CameraPractice/ViewController.swift
+++ CameraPractice/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var newScreen = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newScreen.delegate = self
     }
 
-
+    @IBOutlet weak var photoTaken: UIImageView!
+    
+    @IBAction func openCamera(_ sender: UIButton) {
+        newScreen.sourceType = .camera
+        present(newScreen, animated: true, completion: nil)
+    }
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            photoTaken.image = selectedImage
+        }
+        newScreen.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
